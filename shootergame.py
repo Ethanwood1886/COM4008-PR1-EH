b071a50cd6e23d7b986ab88d8a6bde0e4b04754f
Ethanwood1886 <22319448@bucks.ac.uk>
2023-10-26 15:35:11 +0100

    Code Progress - Addition of background and other images

--- shootergame.py
+++ shootergame.py
@@ -1,17 +1,36 @@
-import pygame
+import pygame  
+import os 
+os.chdir("G:\\GitHub\COM4008-PR1-EH\Shooter_Game_Assets")
+
+pygame.init()
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Army Shooter")
 
+# CONSTANTS
+FPS = 60
+
+
+ARMY_MAN = pygame.image.load("armyman.png")
+POWER_UP_ICON = pygame.image.load("powerupicon.png")
+BACKGROUND = pygame.image.load("grassybackground.jpeg")
+BACKGROUND = pygame.transform.scale(BACKGROUND,(900, 500))
+def draw_images():
+    WIN.blit(ARMY_MAN, (300, 100))
+    pygame.display.update()
+# GAME LOOP
 def main():
-
+    clock = pygame.time.Clock()
     run = True
     while run:
-
+        clock.tick(FPS)
+        WIN.blit(BACKGROUND, (0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+        pygame.display.update()
+        draw_images()
     pygame.quit()
 
 if __name__ == "__main__":
