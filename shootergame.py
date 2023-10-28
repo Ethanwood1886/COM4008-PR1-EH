9170dd22898cb26d0bbf63b58ee4f60942a334a1
hasnaynm <22332992@bucks.ac.uk>
2023-10-28 21:48:44 +0100

    updated code

--- shootergame.py
+++ shootergame.py
@@ -1,6 +1,9 @@
 import pygame  
 import os 
-os.chdir("G:\\GitHub\COM4008-PR1-EH\Shooter_Game_Assets")
+pygame.font.init()
+pygame.mixer.init()
+# os.chdir("G:\\GitHub\COM4008-PR1-EH\Shooter_Game_Assets")
+os.chdir("C:\\Users\\hasna\\OneDrive\\Documents\\GitHub\\COM4008-PR1-EH\\Shooter_Game_Assets")
 
 pygame.init()
 
@@ -8,19 +11,30 @@ WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Army Shooter")
 
+# SOUNDS
+#     FIND SOUND      BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('Shooter_Game_Assets/'))
+#     BULLET_FIRE_SOUND = pygame.mixer.Sound('Shooter_Game_Assets/shoutgunsound.mp3')
+
 # CONSTANTS
 BLACK = (0, 0, 0)
+WHITE = (255,255,255)
 FPS = 60
 VEL = 8
-BORDER = pygame.Rect(WIDTH/2 - 3, 0, 6, HEIGHT)
+BULLET_VEL = 10
+MAX_BULLETS = 4
+HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
+WINNER_FONT = pygame.font.SysFont('comicsans', 100)
+BORDER = pygame.Rect(WIDTH//2 - 3, 0, 6, HEIGHT)
 ARMY_MAN = pygame.image.load("armymanv2.png")
 ARMY_MAN_LEFT = pygame.transform.scale(ARMY_MAN, (100, 125))
 ARMY_MAN_RIGHT = pygame.transform.flip(pygame.transform.scale(ARMY_MAN, (100, 125)), True, False)
 POWER_UP_ICON = pygame.image.load("powerupicon.png")
 BACKGROUND = pygame.image.load("grassybackground.jpeg")
 BACKGROUND = pygame.transform.scale(BACKGROUND,(900, 500))
+LEFT_HIT = pygame.USEREVENT + 1
+RIGHT_HIT = pygame.USEREVENT + 2
 
-
+# KEY CONTROLS & BORDERS 
 def left_movement(keys_pressed, left):
         if keys_pressed[pygame.K_a] and left.x - VEL > 0:
             left.x -= VEL
@@ -41,16 +55,63 @@ def right_movement(keys_pressed, right):
         if keys_pressed[pygame.K_DOWN] and right.y + VEL < HEIGHT - 125:
             right.y += VEL
 
-def draw_window(right, left):
+def draw_window(right, left, left_bullets, right_bullets, right_health, left_health):
     pygame.draw.rect(WIN, BLACK, BORDER)
     WIN.blit(ARMY_MAN_LEFT, (left.x, left.y))
-    WIN.blit(ARMY_MAN_RIGHT, (right.x, right.y))    
+    WIN.blit(ARMY_MAN_RIGHT, (right.x, right.y)) 
+ 
+ # HEALTH TEXT NUMBER 
+    right_health_text = HEALTH_FONT.render("Health: " + str(right_health), 1, WHITE)  
+    left_health_text = HEALTH_FONT.render("Health: " + str(left_health), 1, WHITE)
+    WIN.blit(right_health_text, (WIDTH - right_health_text.get_width() - 10, 10))
+    WIN.blit(left_health_text, (10, 10))
+
+    for bullet in right_bullets: 
+         pygame.draw.rectangle(WIN, (255, 0, 0), bullet)      # RIGHT BULLETS
+
+    for bullet in left_bullets:                               # LEFT BULLETS
+         pygame.draw.rectangle(WIN, (255, 255, 0), bullet)
     pygame.display.update()
+
+
+# MOVE, COLLISION & REMOVAL OF BULLETS
+def handle_bullets(left_bullets, right_bullets, left, right): 
+     for bullet in left_bullets:
+        bullet.x += BULLET_VEL
+        if right.colliderect(bullet):
+               pygame.event.post(pygame.event.Event(RIGHT_HIT))
+               left_bullets.remove(bullet)
+        elif bullet.x > WIDTH:
+             left_bullets.remove(bullet)
+
+     for bullet in right_bullets:
+        bullet.x -= BULLET_VEL
+        if left.colliderect(bullet):
+               pygame.event.post(pygame.event.Event(LEFT_HIT))
+               right_bullets.remove(bullet)
+        elif bullet.x < 0:
+             right_bullets.remove(bullet)
+
+
+def draw_winner(text):
+     draw_text = WINNER_FONT.render(text, 1, WHITE)
+     WIN.blit(draw_text, (WIDTH//2 - draw_text.get_width()//2, HEIGHT//2 -draw_text.get_height()//2))
+     pygame.display.update()
+     pygame.time.delay(4000)
+     
 
 # GAME LOOP
 def main():
     right = pygame.Rect(700, 150, WIDTH, HEIGHT)
     left = pygame.Rect(150, 150, WIDTH, HEIGHT)
+
+  # BULLETS
+    left_bullets = []
+    right_bullets = []
+
+    left_health = 3
+    right_health = 3
+
 
     clock = pygame.time.Clock()
     run = True
@@ -60,12 +121,50 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LCTRL and len(left_bullets) < MAX_BULLETS:     # LEFT BULLET KEY 
+                     bullet = pygame.Rect(left.x + left.width, left.y + left.height//2 - 2, 10, 5)
+                     left_bullets.append(bullet)            
+                     #BULLET_FIRE_SOUND.play()
+
+                if event.key == pygame.K_SPACE and len(right_bullets) < MAX_BULLETS:     # RIGHT BULLET KEY
+                     bullet = pygame.Rect(right.x, right.y + right.height//2 - 2, 10, 5)
+                     right_bullets.append(bullet)
+                     #BULLET_FIRE_SOUND.play()
+# HEALTH
+            if event.type == RIGHT_HIT:
+                 right_health -= 1
+                 #BULLET_HIT_SOUND.play()
+
+
+            if event.type == LEFT_HIT:
+                 left_health -= 1
+                 #BULLET_HIT_SOUND.play()
+                
+
+# WINNING TEXT
+        winner_text = ""
+        if right_health <= 0:
+            winner_text = "Left Player wins!"            
+
+        if left_health <= 0:
+            winner_text = "Right Player wins!"                   
+
+        if winner_text != "":
+            draw_winner(winner_text)
+            break
+
+   
         keys_pressed = pygame.key.get_pressed()      
         left_movement(keys_pressed, left)
         right_movement(keys_pressed, right)
-        draw_window(right, left)
-    pygame.quit()
+        handle_bullets(left_bullets, right_bullets, left, right)
+
+
+        draw_window(right, left, left_bullets, right_bullets, right_health, left_health)
+    main()
 
 if __name__ == "__main__":
     main()
